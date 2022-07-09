4ba60747cb5275c565c58561055a2cf502a26110
Ayesha.Noor <100527908+AyeshaNoorKhan@users.noreply.github.com>
2022-07-09 02:47:22 -0700

    CORS issues2

--- app.js
+++ app.js
@@ -19,13 +19,21 @@ app.use(bodyParser.json());
 
 //CORS POLICY
 app.use(cors());
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+//   );
+//   next();
+// });
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
